f0341dc4ebc1dae04e5b0429544cf253667789d9
Ricardo Amorim <ricardoamorim015@gmail.com>
2024-05-13 18:08:32 -0300

    Atividades

---
+++ Aula2/AT1/index.js
@@ -0,0 +1,11 @@
+var a = prompt('Digite um numero')
+
+
+if (a < 1 ){
+    console.log('Digite um numero maior que 0')
+    }else if (a % 2 == 0 ){
+    console.log(`${a} é um numero par`)
+    }else {
+        console.log(`${a} é um numero impar`)
+    }
+    
---
+++ Aula2/AT2/index2.js
@@ -0,0 +1,12 @@
+var altura = prompt('Digira sua altuda')
+var peso = prompt('Digite seu peso')
+
+var imc = peso / (altura*2)
+
+console.log(imc)
+
+switch (imc){
+    case < 18.5:
+        
+}
+
